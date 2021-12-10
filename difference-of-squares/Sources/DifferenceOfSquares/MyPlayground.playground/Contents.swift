17de44c817322309177cea1d7306c0cc61c5c0ae
Valery <ihValery@icloud.com>
2021-12-10 03:35:13 +0300

    Натуральные числа
    Сейчас бы в школу за парту )))

--- difference-of-squares/Sources/DifferenceOfSquares/MyPlayground.playground/Contents.swift
+++ difference-of-squares/Sources/DifferenceOfSquares/MyPlayground.playground/Contents.swift
@@ -11,15 +11,19 @@ struct Squares {
    let differenceOfSquares: Int
    
    init(_ n: Int) {
-      let sum = (1...n).reduce(0, +)
+      //let sum = (1...n).reduce(0, +)
+      //1+2+3+...n=n(n+1)÷2 доказывается матиндукцией
+      let sum = n * (n + 1) / 2
       squareOfSum = sum * sum
-      sumOfSquares = (1...n).reduce(into: 0) { $0 += ($1 * $1) }
+      //sumOfSquares = (1...n).reduce(into: 0) { $0 += ($1 * $1) }
+      //1^2+2^2+...+n^2=n(n+1)(2n+1)/6 доказывается матиндукцией
+      sumOfSquares = n * (n + 1) * (2 * n + 1) / 6
       differenceOfSquares = squareOfSum - sumOfSquares
    }
 }
 
 
-var square = Squares(100)
+var square = Squares(10000)
 square.squareOfSum
 square.sumOfSquares
 square.differenceOfSquares
