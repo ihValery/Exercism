906084270898d8674cd8b1002e6dd873024dd0c6
Valery <ihValery@icloud.com>
2021-12-11 21:36:27 +0300

    Готово

--- rna-transcription/Sources/RnaTranscription/MyPlayground.playground/Contents.swift
+++ rna-transcription/Sources/RnaTranscription/MyPlayground.playground/Contents.swift
@@ -47,27 +47,30 @@ do {
 */
 
 enum TranscriptionError: Error {
-   case invalidNucleotide
+   case invalidNucleotide(message: String)
 }
 
 struct Nucleotide {
-   let input: String
-   let transcription: [Character: String] = ["G": "C",
-                                             "C": "G",
-                                             "T": "A",
-                                             "A": "U"]
+   private let input: String
+   private let transcription: [Character: String] = ["G": "C",
+                                                     "C": "G",
+                                                     "T": "A",
+                                                     "A": "U"]
    
    init(_ input: String) {
       self.input = input
    }
    
-   func complementOfDNA() -> String {
-      input.reduce("") { output, character in
-         guard let char = transcription[character] else { return "XER" }
+   func complementOfDNA() throws -> String {
+      try input.reduce("") { output, character in
+         
+         guard let char = transcription[character] else {
+            throw TranscriptionError.invalidNucleotide(message: "\(character) is not a valid Nucleotide")
+         }
          
          return output + char
       }
    }
 }
 
-var test = Nucleotide("G").complementOfDNA()
+var test = try Nucleotide("GATCX").complementOfDNA()
--- rna-transcription/Sources/RnaTranscription/RnaTranscription.swift
+++ rna-transcription/Sources/RnaTranscription/RnaTranscription.swift
@@ -1,1 +1,26 @@
+enum TranscriptionError: Error {
+   case invalidNucleotide(message: String)
+}
 
+struct Nucleotide {
+   private let input: String
+   private let transcription: [Character: String] = ["G": "C",
+                                                     "C": "G",
+                                                     "T": "A",
+                                                     "A": "U"]
+   
+   init(_ input: String) {
+      self.input = input
+   }
+   
+   func complementOfDNA() throws -> String {
+      try input.reduce("") { output, character in
+         
+         guard let char = transcription[character] else {
+            throw TranscriptionError.invalidNucleotide(message: "\(character) is not a valid Nucleotide")
+         }
+         
+         return output + char
+      }
+   }
+}
