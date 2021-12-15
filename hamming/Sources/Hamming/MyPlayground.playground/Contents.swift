b22fca8730a4a5dddf2313987663f460f7af0c2d
Valery <ihValery@icloud.com>
2021-12-15 10:28:45 +0300

    Работает

--- hamming/Sources/Hamming/MyPlayground.playground/Contents.swift
+++ hamming/Sources/Hamming/MyPlayground.playground/Contents.swift
@@ -1,6 +1,9 @@
+import UIKit
+
 struct Hamming {
     static func compute(_ one: String, against: String) -> Int? {
         guard one.count == against.count else { return nil }
+//        guard one != "" else { return 0 }
 
         var oneArr = one.map { $0 }
         var twoArr = against.map { $0 }
@@ -15,3 +18,7 @@ struct Hamming {
         return hammingDistance
     }
 }
+
+var hammingDistance = Hamming.compute("12GTR3", against: "12YYY3")
+var hammingDistance3 = Hamming.compute("GGACTGA", against:"GGACTGA")
+var hammingDistance2 = Hamming.compute("", against: "")
