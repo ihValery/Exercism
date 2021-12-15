f8f20a84ba87402423b80ebc537189e88354d7be
Valery <ihValery@icloud.com>
2021-12-15 21:26:45 +0300

    up

--- isogram/Sources/Isogram/Isogram.swift
+++ isogram/Sources/Isogram/Isogram.swift
@@ -1,4 +1,4 @@
 func isIsogram(_ input: String) -> Bool {
-    var onlyLetters = input.lowercased().filter { $0.isLetter }
+    let onlyLetters = input.lowercased().filter { $0.isLetter }
     return onlyLetters.count == Set(onlyLetters).count
 }
