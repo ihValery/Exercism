0438b3936cf7c869d8f91600c0d5a4874c6c7bc7
Valery <ihValery@icloud.com>
2021-12-18 01:00:05 +0300

    Готово

--- nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
+++ nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
@@ -1,1 +1,23 @@
-//Solution goes in Sources
+struct DNA {
+    private var strand: String
+    
+    init? (strand: String) {
+        let nucleotides = ["A", "C", "G", "T"]
+        guard strand.allSatisfy({ nucleotides.contains( String($0) ) }) else { return nil }
+        
+        self.strand = strand
+    }
+    
+    func counts() -> [String: Int] {
+        var result = ["A": 0, "T": 0, "C": 0, "G": 0]
+        
+        for item in strand {
+            result[String(item)]! += 1
+        }
+        return result
+    }
+    
+    func count(_ nucleotid: Character) -> Int {
+        strand.filter { $0 == nucleotid }.count
+    }
+}
