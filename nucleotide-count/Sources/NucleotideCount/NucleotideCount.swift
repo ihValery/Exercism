8745e3df0c6491e09f04c9dbeca9874c87069ae8
Valery <ihValery@icloud.com>
2021-12-18 01:35:58 +0300

    Refactoring

--- nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
+++ nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
@@ -2,20 +2,16 @@ struct DNA {
     private var strand: String
     
     init? (strand: String) {
-        let nucleotides = ["A", "C", "G", "T"]
         guard Set(strand).isSubset(of: "ATCG") else { return nil }
-//        guard strand.allSatisfy({ nucleotides.contains( String($0) ) }) else { return nil }
         
         self.strand = strand
     }
     
     func counts() -> [String: Int] {
-        var result = ["A": 0, "T": 0, "C": 0, "G": 0]
-        
-        for item in strand {
-            result[String(item)]! += 1
-        }
-        return result
+        ["A": count("A"),
+         "T": count("T"),
+         "C": count("C"),
+         "G": count("G")]
     }
     
     func count(_ nucleotid: Character) -> Int {
